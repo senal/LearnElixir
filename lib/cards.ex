c3b16d2c4b840211d2a4c3962e2a0d8f5b048feb
Ranga Fernando <ranga.fernando@gmail.com>
2018-11-16 22:23:13 +1100

    Learn basic concepts of Elixir

---
+++ lib/cards.ex
@@ -0,0 +1,16 @@
+defmodule Cards do
+
+  def create_deck do
+    ["Ace", 
+    "Two", 
+    "Three"]
+  end  
+
+  def shuffle(deck) do 
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
