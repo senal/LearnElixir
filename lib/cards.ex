f03099a8298b750705095ecab7efbe33b7f67cad
Ranga Fernando <ranga.fernando@gmail.com>
2019-01-05 00:08:21 +1100

    Add documents for modules and functions wiht formats

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,12 @@
 defmodule Cards do
 
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+  
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -21,6 +28,18 @@ defmodule Cards do
     :world
   end
 
+@doc """
+  Divides  a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
+
+## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+"""
   def deal(deck, hand_size) do 
     Enum.split(deck, hand_size)
   end
